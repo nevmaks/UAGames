6e1f8cfc681c58121955af27e96369a5181eae2e
Maksym Nevdashchenko <maksym.nevdashchenko@gmail.com>
2016-09-12 08:36:37 +0300

    Change project UR3 Streaming. Create main activity with 2 banners, logo and play/stop buttons. Fixed orientation at portrait position.

--- UR3Streaming/app/src/main/java/ua/uagames/ur3streaming/MainActivity.java
+++ UR3Streaming/app/src/main/java/ua/uagames/ur3streaming/MainActivity.java
@@ -1,13 +1,40 @@
 package ua.uagames.ur3streaming;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player = new MediaPlayer();
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    }
+
+    public void play(View v) {
+        if (!player.isPlaying()) {
+            try {
+                player.setDataSource("http://nrcu.gov.ua:8000/ur3-mp3-l");
+                player.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            player.start();
+        }
+    }
+
+    public void stop(View v) {
+        if (player.isPlaying()) {
+            player.stop();
+            player.reset();
+        }
     }
 }
