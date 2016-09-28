3c5d1311581f51169fd0f3a2c17a7268a4f93d21
Maksym Nevdashchenko <maksym.nevdashchenko@gmail.com>
2016-09-28 21:07:59 +0300

    Add project WeddingITv2016.06.

---
+++ ITWedding/WeddingITv2016.06/app/src/androidTest/java/ua/uagames/wedding/it/v201606/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ua.uagames.wedding.it.v201606;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
