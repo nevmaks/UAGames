82ac4a60dea62a2f3fa83eca74bf053b9c9816cf
Maksym Nevdashchenko <nevmaks@gmail.com>
2016-09-28 22:34:10 +0300

    Add progect Wedding: Ukrainian v201601

---
+++ ITWedding/WeddingUkrainianv2016.01/app/src/main/java/ua/uagames/wedding/ukrainian/v201601/MainActivity.java
@@ -0,0 +1,51 @@
+package ua.uagames.wedding.ukrainian.v201601;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String WEB_BUNDLE = "web_bundle";
+
+    private WebView viewer;
+    private FirebaseAnalytics analytics;
+    private Bundle bundle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        analytics = FirebaseAnalytics.getInstance(this);
+        bundle = new Bundle();
+
+        viewer = (WebView) findViewById(R.id.viewer);
+        viewer.getSettings().setJavaScriptEnabled(true);
+        viewer.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+
+        if (savedInstanceState != null)
+            viewer.restoreState(savedInstanceState.getBundle(WEB_BUNDLE));
+        else
+            viewer.loadUrl("https://drive.google.com/file/d/0B3qWNFQol8lIZzZMSDdVZEhjRmc/view?usp=sharing");
+
+        bundle.putString("app_name", "Wedding: Ukrainian v2016.01.");
+        bundle.putString("app_event", "Create app and load content.");
+        analytics.logEvent("app_statistics", bundle);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Bundle web = new Bundle();
+        viewer.saveState(web);
+        outState.putBundle(WEB_BUNDLE, web);
+        super.onSaveInstanceState(outState);
+    }
+}
